182ab5afd4928b9b0d71cfc25b250daad0f9e4b3
Will Parrish <willptci@gmail.com>
2024-10-07 22:38:56 -0400

    Initial commit

---
+++ types/index.d.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// ========================================
+
+declare type SignUpParams = {
+  firstName: string;
+  lastName: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  email: string;
+  password: string;
+};
+
+declare type LoginUser = {
+  email: string;
+  password: string;
+};
+
+declare type User = {
+  $id: string;
+  email: string;
+  userId: string;
+  dwollaCustomerUrl: string;
+  dwollaCustomerId: string;
+  firstName: string;
+  lastName: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  ssn: string;
+};
+
+declare type NewUserParams = {
+  userId: string;
+  email: string;
+  name: string;
+  password: string;
+};
